40d51fe60e5f1f3765f22cac85caaaa1670872ff
Vamsi Charan Adari <adarivamsi@gmail.com>
2020-04-27 08:02:12 -0400

    First Maven Application.

---
+++ SampleApp/src/main/java/SampleApp.java
@@ -0,0 +1,10 @@
+/**
+ * All Copyrights reserved @ Vamsi Charan Adari
+ * Any queries mail me at adarivamsi@gmail.com
+ * Created by adari on 4/27/2020
+ */
+public class SampleApp {
+    public static void main(String[] args) {
+        System.out.println("Sample App");
+    }
+}
